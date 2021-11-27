e73c53bf9a4a7cfcefb9b8d98541c10df62c98e2
Armando Calderon <93158352+Arman3107@users.noreply.github.com>
2021-11-27 13:38:51 -0600

    Documentación del código main

--- Discord.py
+++ Discord.py
@@ -1,12 +1,14 @@
+#Codigo main de ejecucion del codigo principal
 import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 import Musica
-
+#Se exportan las distintas librerias utilizadas en el codigo fuente 
 cogs = [Musica]
 
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+#Ejecucion del comando ! en discord 
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
@@ -14,3 +16,6 @@ for i in range(len(cogs)):
 
 
 client.run("OTEwNzIxNzkyOTA4MzQ5NDUw.YZW9ww.jsp6z_BGYWBw8mIrmvjvJoBIVkc")
+#Se utiliza el token entregado por el discord developer para la ejecucion del codigo
+#El token es de uso personal, si se quiere utilizar el bot debes usar un token generado
+#de forma personal en el discord developer
